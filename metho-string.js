610ccb0497b1525e3a0dfd0d2b226ed969ce89e0
Jon Randy <jonrandy@gmail.com>
2022-12-13 09:50:43 +0700

    Resulting changes from `reverse` change in metho-array

--- metho-string.js
+++ metho-string.js
@@ -62,12 +62,29 @@ export const proper = Metho.add(target, function proper() {
 	})
 })
 
-// reverse - reverse the string
+// // reverse - reverse the string
+// export const reverse = Metho.addWithSharedSymbolName(
+// 	target,
+// 	function reverse() {
+// 		return [...this].reverse().join("")
+// 	},
+// 	"arrayOrStringReverse"
+// )
+// reverse - reverse the array (duplicate native method - but don't mutate unless asked) or string
+const reverseFunc = function reverse(mutate) {
+	let res
+	if (this[Metho.data] == ARRAY_FLAG) {
+		// reverse array
+		res = mutate ? this.reverse() : [...this].reverse()
+	} else if (this[Metho.data] == STRING_FLAG) {
+		// reverse string
+		res = [...this].reverse().join("")
+	}
+	return res
+}
 export const reverse = Metho.addWithSharedSymbolName(
 	target,
-	function reverse() {
-		return [...this].reverse().join("")
-	},
+	reverseFunc,
 	"arrayOrStringReverse"
 )
 
